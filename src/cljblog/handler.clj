159e685a93f08b16255cd06358af3ca6eed03fc4
Martin Haesler <martin.haesler@gmail.com>
2013-12-18 13:55:43 +0000

    Forgot clojure needs definit. to come before being used.

--- src/cljblog/handler.clj
+++ src/cljblog/handler.clj
@@ -1,14 +1,26 @@
 (ns cljblog.handler
-  (:use compojure.core hiccup.core)
+  (:use compojure.core hiccup.core hiccup.util)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
 
+(def page-header
+  (html
+   [:head [:title "this is the title"]]
+   [:body
+    [:p "some Text"]]))
+
 (defroutes app-routes
-  (GET "/" [] some-text)
+  (GET "/" [] page-header)
   (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
   (handler/site app-routes))
+
+
+
+
+
+
 
 
