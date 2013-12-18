f9cc13a9d9bc4da78c27791265b213f525f138f5
Martin Haesler <martin.haesler@gmail.com>
2013-12-18 17:35:52 +0000

    Added hiccup.page for include-js/css.

--- src/cljblog/handler.clj
+++ src/cljblog/handler.clj
@@ -1,18 +1,43 @@
 (ns cljblog.handler
-  (:use compojure.core hiccup.core hiccup.util)
+  (:use compojure.core hiccup.core hiccup.util hiccup.page)
   (:require [compojure.handler :as handler]
             [compojure.route :as route]))
 
-(def page-header
+(defn page-header
+  "return html-string for header of html file
+   with entered title"
+  [title]
   (html
-   [:head [:title "this is the title"]]
+   [:head
+    [:title title]
+    (include-js "http://code.jquery.com/jquery-latest.min.js")
+    (include-css "/blog.css")]))
+
+(def newpost-page-body
+  (html5
    [:body
-    [:p "some Text"]]))
+    [:h2 {:class "header"} "Enter a new Post."]
+    [:div {:class "wholeform"}
+     [:form {:method "post"}
+      [:div {:class "tinput"}
+       [:input {:type "text" :class "tfeld" :name "text"}]]
+      [:div {:class "tarea"}
+       [:textarea {:class "tarea" :name "tarea"}]]
+      [:div {:class "button"}
+       [:input {:type "submit" :text "submit"}]]]]]))
+
+(def newpost-page
+  (apply str (page-header "New post") newpost-page-body))
+
+ 
 
 (defroutes app-routes
-  (GET "/" [] page-header)
+  (GET "/" [] newpost-page)
   (route/resources "/")
-  (route/not-found "Not Found"))
+  (route/not-found "Not Found")
+  (POST "/newpost" [] newpost-page)
+  (GET "/newpost" [] newpost-page)
+  (route/resources "/newpost"))
 
 (def app
   (handler/site app-routes))
